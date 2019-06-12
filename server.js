a29ea52a761a5478a7950f68b3419e4bce3e1d1a
Nomad9-0 <ojimenezlmpc@gmail.com>
2019-06-11 19:11:26 -0700

    Restructure to simpler version. Need to get Clear button to work

--- server.js
+++ server.js
@@ -1,39 +1,140 @@
-// Dependencies
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const axios = require("axios");
-const cheerio = require("cheerio");
-const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
+var express = require("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 // Initialize Express
-const app = express();
+var app = express();
 
-app.use(logger("dev"));
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+app.use(express.static("public"));
+var PORT = process.env.PORT || 3000;
+var db = require("./models");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsdb";
 
-app.use(express.static(process.cwd() + "/public"));
-
-// Handlebars
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set("view engine", "handlebars");
-
-// Mongoose connection
-const MONGODB_URI = (process.env.MONGODB_URI || "mongodb://localhost/News-Scraping");
 mongoose.connect(MONGODB_URI);
-const db = mongoose.connection;
-// db err catcher
-db.on('error', console.error.bind(console, "connection error"));
-db.once("open", function() {
-  console.log("Connected to Mongoose");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.News.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
 });
 
-// Port
-app.listen(3000, function() {
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  // Make a request via axios for the news section of `ycombinator`
+  axios.get("https://www.pcgamer.com").then(function(response) {
+    // Load the body of the HTML into cheerio
+    var $ = cheerio.load(response.data);
+
+    // Empty array to save our scraped data
+    var scrapeResults = [];
+    console.log("scraped");
+    $("article").each(function(i, element) {
+      // Article data
+
+      var title = $(this)
+        .find("h3")
+        .text();
+      var url = $(this)
+        .parent("a")
+        .attr("href");
+      var img = $(this)
+        .find("img")
+        .attr("src");
+
+      var newsData = {
+        title: title,
+        url: url,
+        img: img
+      };
+
+      db.News.create(newsData)
+        .then(dbNews => console.log(dbNews))
+        .catch(err => res.json(err));
+      console.log(scrapeResults);
+    });
+  });
+  res.redirect("/");
+});
+// Send a "Scrape Complete" message to the browser
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.News.find({})
+    .then(function(dbNews) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbNews);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// GET Route for grabbing an article by its specific ID and adding a note
+app.get("/articles/:id", (req, res) => {
+  db.News.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(dbNews => res.json(dbNews))
+    .catch(err => res.json(err));
+});
+
+// Route for saving/updating an Article's associated Note
+// POST Route for saving an Article's note
+app.post("/articles/:id", (req, res) => {
+  db.Note.create(req.body)
+    .then(dbNote => {
+      console.log("article returned");
+      return db.News.findOneAndUpdate(
+        { _id: req.params.id },
+        { saved: true, note: dbNote._id },
+        { new: true }
+      );
+    })
+    .then(dbNews => {
+      res.redirect("/");
+      res.json(dbNews);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//Delete
+app.delete("/clear", function(req, res) {
+  db.News.drop(newsData)
+  console.log("Deleted");
+});
+
+
+// DELETE route for deleting posts
+app.delete("/articles/delete/:id", function(req, res) {
+  console.log(req.params.id);
+  db.News.findByIdAndRemove({
+    _id: req.params.id
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// Listen on port 3000
+app.listen(PORT, function() {
   console.log("App running on port 3000!");
 });
